d5cfd9352363123ed335f63f8c0ee3cde0640fe0
EsportsKing <divpadsmitais@gmail.com>
2020-10-27 11:36:04 +0200

    Finished Task 4

--- src/selenium/sample/Sample4Task.java
+++ src/selenium/sample/Sample4Task.java
@@ -37,36 +37,53 @@ public class Sample4Task {
 //        enter a number under "Number"
         String numberKey = "4";
         WebElement textArea = driver.findElement(By.id("number"));
-        //WebElement numberBox = driver.findElement(By.id("number"));
-        System.out.println(textArea.getText());
-        System.out.println(textArea.getAttribute("value"));
-//        System.out.println("Help");
+        textArea.clear();
+        textArea.sendKeys(numberKey); // sending keys "4"
+        assertEquals(textArea.getAttribute("value"), numberKey); // checking that textArea is "4"
 
-        //System.out.println(numberBox.getCssValue());
-//
-//        numberBox.clear();
-//        assertEquals(numberBox.getText(), ""); // checking that getText is empty
-//        assertEquals(numberBox.getAttribute("value"), ""); // checking that value is also empty
-//
-//        numberBox.sendKeys(numberKey);
-//        assertEquals(numberBox.getText(), "4"); // checking that getText is empty
-//        assertEquals(numberBox.getAttribute("value"), "4"); // checking that value is also empty
 //        check that button is not clickable "Clear Result"
+        WebElement clearResultButton = driver.findElement(By.id("clear_result_button_number"));
+        assertFalse(clearResultButton.isEnabled());
+
 //        check that text is not displayed
+        WebElement resultNumber = driver.findElement(By.id("result_number"));
+        assertFalse(resultNumber.isDisplayed());
+
 //        click on "Result" button
+        WebElement resultButton = driver.findElement(By.id("result_button_number"));
+        resultButton.click();
+
 //        check that text is displayed
+        assertTrue(resultNumber.isDisplayed());
+
 //        check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
+        assertEquals("You entered number: \"" + numberKey + "\"",resultNumber.getText());
+
 //        check that the button "Clear Result" is clickable now
+        assertTrue(clearResultButton.isEnabled());
+
 //        click on "Clear Result"
+        clearResultButton.click();
+
 //        check that the text is still (""), but it is not displayed
+        System.out.println(resultNumber.getText());
+        assertEquals("", resultNumber.getText());
+        assertFalse(resultNumber.isDisplayed());
     }
 
     @Test
     public void clickOnLink() throws Exception {
-//         TODO:
 //        check current url is base_url
+        assertEquals(base_url, driver.getCurrentUrl());
+
 //        click on "This is a link to Homepage"
+        WebElement homepageLink = driver.findElement(By.id("homepage_link"));
+        homepageLink.click();
+
 //        check that current url is not base_url
+        assertNotEquals(base_url, driver.getCurrentUrl());
+
 //        verify that current url is homepage
+        assertEquals("https://kristinek.github.io/site/", driver.getCurrentUrl());
     }
 }
